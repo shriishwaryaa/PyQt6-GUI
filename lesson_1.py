e1deac18e33ea53f0f8130e505ae442849f13124
Shri Ishwaryaa S V <ishwaryaa555@gmail.com>
2023-02-02 17:17:51 -0500

    Created a custom window with a window title and a push button

--- lesson_1.py
+++ lesson_1.py
@@ -1,8 +1,21 @@
-from PyQt6.QtWidgets import QApplication, QMainWindow
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+
+# Subclass QMainWindow to customize the main window
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My GUI")
+        button = QPushButton("Press Me!")
+
+        # Set the central widget of the Window
+        self.setCentralWidget(button)
+
 
 app = QApplication([])
 
-window = QMainWindow()
+window = MainWindow()
 window.show()
 
 app.exec()
