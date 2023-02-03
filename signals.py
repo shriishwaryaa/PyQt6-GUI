5dbf28a4adaaab6fa4dad5a8143aa83a13a1dd76
Shri Ishwaryaa S V <ishwaryaa555@gmail.com>
2023-02-03 13:01:07 -0500

    Implemented .contextMenuEvent event handler

--- signals.py
+++ signals.py
@@ -1,36 +1,20 @@
 import sys
 
 from PyQt6.QtCore import Qt
-from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QTextEdit
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import QApplication, QLabel, QMainWindow, QMenu
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.label = QLabel("Click in this window")
-        self.setCentralWidget(self.label)
 
-    def mouseMoveEvent(self, e):
-        self.label.setText("mouseMoveEvent")
-
-    def mousePressEvent(self, e):
-        if e.button() == Qt.MouseButton.LeftButton:
-            # handle the left-button press in here
-            self.label.setText("mousePressEvent LEFT")
-
-        elif e.button() == Qt.MouseButton.MiddleButton:
-            # handle the middle-button press in here.
-            self.label.setText("mousePressEvent MIDDLE")
-
-        elif e.button() == Qt.MouseButton.RightButton:
-            # handle the right-button press in here.
-            self.label.setText("mousePressEvent RIGHT")
-
-    def mouseReleaseEvent(self, e):
-        self.label.setText("mouseReleaseEvent")
-
-    def mouseDoubleClickEvent(self, e):
-        self.label.setText("mouseDoubleClickEvent")
+    def contextMenuEvent(self, e):
+        context = QMenu(self)
+        context.addAction(QAction("test 1", self))
+        context.addAction(QAction("test 2", self))
+        context.addAction(QAction("test 3", self))
+        context.exec(e.globalPos())
 
 
 app = QApplication(sys.argv)
