b750e791bdc49daadeab1b0542895fd23b50bb7e
Shri Ishwaryaa S V <ishwaryaa555@gmail.com>
2023-02-03 11:40:36 -0500

    Implemented the .windowTitleChanged signal

--- signals.py
+++ signals.py
@@ -1,24 +1,47 @@
 import sys
+from random import choice
 from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
+
+window_titles = [
+    'My App',
+    'My App',
+    'Still My App',
+    'Still My App',
+    'What on earth',
+    'What on earth',
+    'This is surprising',
+    'This is surprising',
+    'Something went wrong'
+]
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
+        self.n_times_clicked = 0
+
         self.setWindowTitle("My GUI")
 
         self.button = QPushButton("Press Me!")
         self.button.clicked.connect(self.button_click)
 
+        self.windowTitleChanged.connect(self.window_title_change)
+
         # Set the central widget of the Window.
         self.setCentralWidget(self.button)
 
     def button_click(self):
-        self.button.setText("You already pressed me!")
-        self.button.setEnabled(False)
+        print("Clicked!")
+        new_window_title = choice(window_titles)
+        print("Setting title:  %s" % new_window_title)
+        self.setWindowTitle(new_window_title)
 
-        self.setWindowTitle("My GUI changes")
+    def window_title_change(self, window_title):
+        print("Window title changed: %s" % window_title)
+
+        if window_title == 'Something went wrong':
+            self.button.setDisabled(True)
 
 
 app = QApplication(sys.argv)
