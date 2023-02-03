257b715a9032fa07be816ae52d983d093a321fbc
Shri Ishwaryaa S V <ishwaryaa555@gmail.com>
2023-02-03 12:45:45 -0500

    Implemented usage of mouse event objects

--- signals.py
+++ signals.py
@@ -14,7 +14,17 @@ class MainWindow(QMainWindow):
         self.label.setText("mouseMoveEvent")
 
     def mousePressEvent(self, e):
-        self.label.setText("mousePressEvent")
+        if e.button() == Qt.MouseButton.LeftButton:
+            # handle the left-button press in here
+            self.label.setText("mousePressEvent LEFT")
+
+        elif e.button() == Qt.MouseButton.MiddleButton:
+            # handle the middle-button press in here.
+            self.label.setText("mousePressEvent MIDDLE")
+
+        elif e.button() == Qt.MouseButton.RightButton:
+            # handle the right-button press in here.
+            self.label.setText("mousePressEvent RIGHT")
 
     def mouseReleaseEvent(self, e):
         self.label.setText("mouseReleaseEvent")
