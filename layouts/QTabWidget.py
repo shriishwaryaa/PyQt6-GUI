b66a340a7f42abe9ab7d599dac2b5082fe07434a
Shri Ishwaryaa S V <ishwaryaa555@gmail.com>
2023-02-04 17:55:59 -0500

    Implemented tab interface using layouts and QTabWidget

---
+++ layouts/QTabWidget.py
@@ -0,0 +1,38 @@
+import sys
+
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QTabWidget
+from PyQt6.QtGui import QPalette, QColor
+
+
+class Color(QWidget):
+    def __init__(self, color):
+        super(Color, self).__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.ColorRole.Window, QColor(color))
+        self.setPalette(palette)
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowTitle("My App")
+
+        tabs = QTabWidget()
+        tabs.setMovable(True)
+        tabs.setDocumentMode(True)
+
+        for n, color in enumerate(["red", "green", "blue", "yellow"]):
+            tabs.addTab(Color(color), color)
+
+        self.setCentralWidget(tabs)
+
+
+app = QApplication(sys.argv)
+
+window = MainWindow()
+window.show()
+
+app.exec()
